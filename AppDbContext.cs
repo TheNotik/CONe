3fe512fea1d1e93fc0bb559521d06c9d43da3d95
TheNotik <adikxxar@gmail.com>
2026-06-09 07:48:15 +0600

    Add files via upload

---
+++ AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Producer.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Person> Persons { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=persons.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>().HasData(
+                new Person { Id = 1, Pin = "12345678901234", Name = "Мухамбетов Амир Алмазович", Gender = Gender.Male, BirthDate = new DateTime(2007, 9, 25) },
+                new Person { Id = 2, Pin = "98765432109876", Name = "Анарбаев Адилет Максатбекович", Gender = Gender.Male, BirthDate = new DateTime(2007, 12, 1) }
+            );
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Producer.Data
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Pin { get; set; }
+        public string Name { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public byte[]? Photo { get; set; }  // ← добавь сюда
+    }
+}
+
